c5fc5ac728ee933a52cab59a376476322aea81c1
dannertech <danner@danner.tech>
2023-04-16 11:28:28 -0400

    confirmed Button adds new blog post to blog posts array

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -3,14 +3,33 @@ import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import {Button, TextInput} from '@react-native-material/core';
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 import { BlogContext } from '../context/BlogContext';
+//import {SuccessAnimation} from 'react-native-success-animation';
 
 const CreateScreen = () => {
     const Context = useContext(BlogContext);
     const [text, setText] = useState('');
+    const [postSuccess, setPostSuccess] = useState(false);
+
+    const submitPost = () => {
+        return text.length > 5 ? Context.dispatch({type: 'ADD_POST', payload: text}) : null
+    }
     return(
         <View style={styles.mainView}>
+            {/* {postSuccess && (
+                <SuccessAnimation 
+                size={120}
+                iconSize={120 * 0.7}
+                iconColor='white'
+                dotColor='#44c6b1'
+                dotSize={20}
+                duration={2000}
+                backgroundColor='#44c6b1'
+                animatedLayerColor='white'
+                onAnimationEnd={() => setPostSuccess(false)}
+                />
+            )} */}
             
-            <TextInput placeholder="What's on your mind?" onChangeText={(text) =>setText(text)} 
+            <TextInput placeholder="What's on your mind?" onChangeText={(text) => setText(text)} 
             leading={() => {
                 return(
                     <MaterialCommunityIcons name="brain" size={24} color="black"/>
@@ -19,8 +38,8 @@ const CreateScreen = () => {
             />
             <View style={styles.submitButtonView}>
             <Button title="Add Blog Post" onPress={() => {
-                        Context.dispatch({type: 'ADD_POST', payload: `Blog Post #${Context.state.length + 1}`})
-                        
+                        submitPost()
+                        setPostSuccess(true);
                     }} />
             </View>
         </View>
