d91cb10c6d27ea3089d5c2ef973326a1c2e6e387
dannertech <danner@danner.tech>
2023-04-16 10:50:33 -0400

    centered components n CreateScreen. Keep going Diamond

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,21 +1,36 @@
 import React, {useContext, useState} from 'react';
 import {StyleSheet, View, Text, TouchableOpacity, TextInput} from 'react-native';
+import {Button} from '@react-native-material/core';
 import { BlogContext } from '../context/BlogContext';
 
 const CreateScreen = () => {
     const Context = useContext(BlogContext);
     const [text, setText] = useState('');
     return(
-        <View>
+        <View style={styles.mainView}>
             <Text>Create Screen</Text>
             <Text>Enter Blog Post</Text>
             <TextInput placeholder="Enter Post Here" onChangeText={(text) =>setText(text)}/>
-            <TouchableOpacity onPress={() => {
+            <View style={styles.submitButtonView}>
+            <Button title="Add Blog Post" onPress={() => {
                         Context.dispatch({type: 'ADD_POST', payload: `Blog Post #${Context.state.length + 1}`})
                         
-                    }}><Text>Add Blog Post</Text></TouchableOpacity>
+                    }} />
+            </View>
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    submitButtonView: {
+        marginVertical: 20,
+    },
+    mainView: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+    }
+})
 
 export default CreateScreen;
