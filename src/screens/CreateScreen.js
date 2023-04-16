7c88d10686ccd40195b08e504d0a75078e75382b
dannertech <danner@danner.tech>
2023-04-16 10:31:46 -0400

    added MaterialUI to application

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,13 +1,15 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {StyleSheet, View, Text, TouchableOpacity, TextInput} from 'react-native';
 import { BlogContext } from '../context/BlogContext';
 
 const CreateScreen = () => {
     const Context = useContext(BlogContext);
-    
+    const [text, setText] = useState('');
     return(
         <View>
             <Text>Create Screen</Text>
+            <Text>Enter Blog Post</Text>
+            <TextInput placeholder="Enter Post Here" onChangeText={(text) =>setText(text)}/>
             <TouchableOpacity onPress={() => {
                         Context.dispatch({type: 'ADD_POST', payload: `Blog Post #${Context.state.length + 1}`})
                         
